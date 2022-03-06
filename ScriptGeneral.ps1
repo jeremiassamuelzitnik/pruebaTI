47abe851515efa7101489d2f78384eb40dcbce4c
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2022-03-06 15:28:50 -0300

    Update ScriptGeneral.ps1

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -1,3 +1,3 @@
 wget https://drive.mistrelci.com.ar/archivos//General.zip -OutFile $env:TEMP\JeremosTKBio\General.zip
-Expand-Archive -LiteralPath $env:TEMP\JeremosTKBio\General.Zip -DestinationPath $env:TEMP\JeremosTKBio\
+Expand-Archive -Path $env:TEMP\JeremosTKBio\General.Zip -DestinationPath $env:TEMP\JeremosTKBio\
 Start-Process  $env:TEMP\JeremosTKBio\General\script` general.cmd
