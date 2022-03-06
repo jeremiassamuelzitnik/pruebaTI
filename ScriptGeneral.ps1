887a164a0ec100502e6674c2f05cdf4881edf9ff
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2022-03-06 16:03:33 -0300

    Update ScriptGeneral.ps1

--- ScriptGeneral.ps1
+++ ScriptGeneral.ps1
@@ -1,5 +1,3 @@
 wget https://raw.githubusercontent.com/jeremiassamuelzitnik/pruebaTI/master/General.zip -OutFile $env:TEMP\JeremosTKBio\General.zip
 Expand-Archive -Path $env:TEMP\JeremosTKBio\General.Zip -DestinationPath $env:TEMP\JeremosTKBio\ -force
 Start-Process  $env:TEMP\JeremosTKBio\General\script` general.cmd
-
-
