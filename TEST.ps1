660cce4a4883c35e53bbfca6b9b2c05dccaf961e
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2022-03-06 15:55:22 -0300

    Create TEST.ps1

---
+++ TEST.ps1
@@ -0,0 +1,3 @@
+wget https://github.com/jeremiassamuelzitnik/pruebaTI/edit/master/General.zip -OutFile $env:TEMP\JeremosTKBio\General.zip
+Expand-Archive -Path $env:TEMP\JeremosTKBio\General.Zip -DestinationPath $env:TEMP\JeremosTKBio\ -force
+Start-Process  $env:TEMP\JeremosTKBio\General\script` general.cmd
