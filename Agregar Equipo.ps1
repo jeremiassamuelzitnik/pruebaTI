e7ab090919abdc242c0250a721a098e3c50c6cc5
jeremiassamuelzitnik <69435115+jeremiassamuelzitnik@users.noreply.github.com>
2022-03-06 15:14:11 -0300

    Delete Agregar Equipo.ps1

--- Agregar Equipo.ps1
+++
@@ -1,5 +0,0 @@
-if (-not (Test-Path -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel))
-			{
-				New-Item -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel -Force
-			}
-			New-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -PropertyType DWord -Value 0 -Force
